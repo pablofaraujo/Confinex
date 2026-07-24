b8f4a64ec5ffa8344408ee990547e6a83b47fa35
Pablo Ferreira Araujo <108636348+pablofaraujo@users.noreply.github.com>
2026-07-24 07:23:42 -0600

    Desambigua destino do Portfólio B3 na auditoria

--- js/cfagro-shell.js
+++ js/cfagro-shell.js
@@ -21,7 +21,7 @@ var NAV = [
   { href:'./confinamento.html', rotulo:'Confinamento', icone:'🐮' },
   { href:'./fazenda-ametista.html', rotulo:'Fazenda Ametista', icone:'🌾' },
   // O portfólio B3 usa a tela BGI local para preservar o shell e a navegação
-  { href:'./bgi.html',      rotulo:'Portfolio B3', icone:'🗂' },
+  { href:'./bgi.html?visao=portfolio', rotulo:'Portfolio B3', icone:'🗂' },
   { href:'./bgi.html',      rotulo:'BGI',           icone:'📈' },
   { href:'./bb.html',       rotulo:'Boi Balança',   icone:'⚖️' },
   { href:'./abate.html',    rotulo:'Abate',         icone:'🥩' },
